73af4a0bc029e7ec7b18bf9e28ae09f9c0eeb9f7
LPranavP <lqp5348@psu.edu>
2024-10-12 22:41:36 -0400

    added backend stuff

---
+++ coursebuilderBE/src/main/kotlin/com/thatsussygroup/coursebuilder/CoursebuilderApplication.kt
@@ -0,0 +1,11 @@
+package com.thatsussygroup.coursebuilder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoursebuilderApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoursebuilderApplication>(*args)
+}
---
+++ coursebuilderBE/src/test/kotlin/com/thatsussygroup/coursebuilder/CoursebuilderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.thatsussygroup.coursebuilder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoursebuilderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
