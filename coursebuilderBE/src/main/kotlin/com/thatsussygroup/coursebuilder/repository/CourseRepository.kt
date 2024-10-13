3cf294a2a23c90d397f864eeebb779f196508820
LPranavP <lqp5348@psu.edu>
2024-10-13 13:07:11 -0400

    added repository annotations

--- coursebuilderBE/src/main/kotlin/com/thatsussygroup/coursebuilder/repository/CourseRepository.kt
+++ coursebuilderBE/src/main/kotlin/com/thatsussygroup/coursebuilder/repository/CourseRepository.kt
@@ -2,7 +2,9 @@ package com.thatsussygroup.coursebuilder.repository
 
 import com.thatsussygroup.coursebuilder.model.Course
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface CourseRepository : MongoRepository<String, Course> {
     fun findByName(name: String): Course
     fun findByTypeOfCourse(typeOfCourse: String): List<Course>
--- coursebuilderBE/src/main/kotlin/com/thatsussygroup/coursebuilder/repository/FeedbackRepository.kt
+++ coursebuilderBE/src/main/kotlin/com/thatsussygroup/coursebuilder/repository/FeedbackRepository.kt
@@ -2,7 +2,9 @@ package com.thatsussygroup.coursebuilder.repository
 
 import com.thatsussygroup.coursebuilder.model.Feedback
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface FeedbackRepository : MongoRepository<String, Feedback> {
     fun findByProfessor(name: String): Feedback
     fun findByRating(rating: Double): Feedback
